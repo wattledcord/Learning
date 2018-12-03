76f08372b7bf0dc29dbc00f71c935128ac5dfee8
B Madhava Reddy <bmadhavreddy2015@gmail.com>
2018-12-03 14:10:29 +0530

    Added Materail UI and removed Office Fabric UI

--- client/src/Pages/App.js
+++ client/src/Pages/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import { TextField } from 'office-ui-fabric-react/lib/TextField';
-import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import axios from 'axios';
 
 class App extends Component {
@@ -40,19 +40,18 @@ class App extends Component {
   render() {
 
     return (
-      <div className="App">
-        <div className="grid">
-          <TextField label="Employee Name" name="EmployeeName" required onChange={this.handleInputChange} />
-          <TextField label="First Name" name="FirstName" onChange={this.handleInputChange}  required /> 
-          <TextField label="Last Name" name="LastName" onChange={this.handleInputChange} />
-          <TextField label="Phone Number" name="PhoneNumber" onChange={this.handleInputChange} />
-          <TextField label="Address" multiline rows={4} name="Address" onChange={this.handleInputChange} />
-          <TextField label="Company" name="Company" onChange={this.handleInputChange} />
-          <TextField label="Designation" name="Designation" onChange={this.handleInputChange} />
-          <TextField label="HighestEducation" name="HighestEducation" onChange={this.handleInputChange} />
-          <br />
-          <PrimaryButton text="Submit" onClick={this.handleFormSubmitClick} />
-        </div>
+      <div className="App">        
+          <form>
+            <TextField label="Employee Name" name="EmployeeName" required onChange={this.handleInputChange} fullWidth variant="outlined" margin="normal" />
+            <TextField label="First Name" name="FirstName" onChange={this.handleInputChange} required fullWidth variant="outlined" margin="normal"/>
+            <TextField label="Last Name" name="LastName" onChange={this.handleInputChange} fullWidth variant="outlined" margin="normal"/>
+            <TextField label="Phone Number" name="PhoneNumber" onChange={this.handleInputChange} fullWidth variant="outlined" margin="normal"/>
+            <TextField label="Address" multiline rows={4} name="Address" onChange={this.handleInputChange} fullWidth variant="outlined" margin="normal"/>
+            <TextField label="Company" name="Company" onChange={this.handleInputChange} fullWidth variant="outlined" margin="normal"/>
+            <TextField label="Designation" name="Designation" onChange={this.handleInputChange} fullWidth variant="outlined" margin="normal"/>
+            <TextField label="HighestEducation" name="HighestEducation" onChange={this.handleInputChange} fullWidth variant="outlined" margin="normal"/>
+            <Button onClick={this.handleFormSubmitClick} color="primary" variant="contained" fullWidth>Submit</Button>
+          </form>        
       </div>
     )
   }
--- client/src/Pages/ViewEmployee.js
+++ client/src/Pages/ViewEmployee.js
@@ -1,23 +1,106 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-class ViewEmployee extends Component{
-    constructor(props){
+import Paper from "@material-ui/core/Paper";
+import {  Table, TableBody, TableRow, TableCell, TextField, Button, Snackbar } from '@material-ui/core';
+
+class ViewEmployee extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             EmployeeName: '', FirstName: '', LastName: '', PhoneNumber: '', Address: '',
             Company: '',
-            Designation: '', HighestEducation: ''
-          }
-       
+            Designation: '', HighestEducation: '',
+            ID: '',snackopen:false,snackmsg:''
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleFormSubmitClick = this.handleFormSubmitClick.bind(this);
     }
-    componentWillMount(){
-        axios.get("http://localhost:8080/employee/1").then((response)=>{                       
-            this.setState({EmployeeName:response.data.EmployeeName,FirstName:response.data.FirstName})
+    handleInputChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+    handleFormSubmitClick(event) {
+        const url = `http://localhost:8080/employee/${this.state.ID}`;
+        axios.get(url).then((response) => {
+            if (response.data.EmployeeName)
+                this.setState({
+                    EmployeeName: response.data.EmployeeName, FirstName: response.data.FirstName, LastName: response.data.LastName,
+                    PhoneNumber: response.data.PhoneNumber, Address: response.data.Address,
+                    Company: response.data.Company, Designation: response.data.Designation,
+                    HighestEducation: response.data.HighestEducation
+                })
+            else{                
+                console.log(response.data)
+                this.setState({
+                    snackmsg:response.data,
+                    EmployeeName: '', FirstName: '', LastName: '',
+                    PhoneNumber: '', Address: '',
+                    Company: '', Designation: '',
+                    HighestEducation: ''
+                })
+this.setState({
+    snackopen:true
+})
+            }
+                
         })
     }
-    render(){                
+    componentWillMount() {
+
+    }
+    render() {
         return (
-<div>{this.state.EmployeeName}-{this.state.FirstName}</div>
+            <div>
+                <form>
+                    <TextField label="Employee ID" name="ID" required onChange={this.handleInputChange} fullWidth variant="outlined" margin="normal" />
+                    <Button onClick={this.handleFormSubmitClick} color="primary" variant="contained" fullWidth>Submit</Button>
+                </form>
+                <br />
+                <Paper>
+                    <Table >
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>Employee Name</TableCell>
+                                <TableCell>{this.state.EmployeeName}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>First Name</TableCell>
+                                <TableCell>{this.state.FirstName}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Last Name</TableCell>
+                                <TableCell>{this.state.LastName}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell>Phone Number</TableCell>
+                                <TableCell>{this.state.PhoneNumber}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell>Address</TableCell>
+                                <TableCell>{this.state.Address}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell>Company</TableCell>
+                                <TableCell>{this.state.Company}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell>Designation</TableCell>
+                                <TableCell>{this.state.Designation}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                <TableCell>Highest Education</TableCell>
+                                <TableCell>{this.state.HighestEducation}</TableCell>
+                                </TableRow>
+
+                        </TableBody>
+                    </Table>
+                </Paper>
+                <Snackbar anchorOrigin={{
+                    vertical:'top',
+                    horizontal:'center'
+                    
+                }} open={this.state.snackopen} autoHideDuration={1000}
+                ></Snackbar>
+            </div>
         )
     }
 }
