87aa16b3c3bc694e72048d4730e9c69281502eb9
B Madhava Reddy <bmadhavreddy2015@gmail.com>
2018-12-13 21:45:43 +0530

    Created Music Player Widget

--- Software/JavaScript/OneDrive App/graph-tutorial/routes/index.js
+++ Software/JavaScript/OneDrive App/graph-tutorial/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const path =require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,5 +10,9 @@ router.get('/', function(req, res, next) {
 
   res.render('index', params);
 });
+router.get('/public/images/:imagename',(req,res)=>{
+  console.log("Got into here",req.params.imagename);
+  res.sendFile(path.resolve(__dirname,`../public/images/${req.params.imagename}`));
+})
 
 module.exports = router;
