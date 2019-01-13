22198c593181873490816bef80fff4328cd875e2
B Madhava Reddy <bmadhavreddy2015@gmail.com>
2019-01-13 15:12:49 +0530

    Added Excel Support

--- Software/Java/Java Relearn/src/Base.java
+++ Software/Java/Java Relearn/src/Base.java
@@ -1,5 +1,19 @@
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
 public class Base {
     public static void main(String[] args) {
         System.out.println("Hello World Again");
+        Workbook wb=new HSSFWorkbook();
+        try(OutputStream fileout=new FileOutputStream("workbook.xls")){
+            wb.write(fileout);
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
     }
+
 }
